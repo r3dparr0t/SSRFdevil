23c1698311bb046bbf75fba3946efba2c4471932
redParrot <36606386+r3dparr0t@users.noreply.github.com>
2026-07-10 04:35:13 +0000

    Update delay_engine.rs delay range optimised.

--- src/engine/delay_engine.rs
+++ src/engine/delay_engine.rs
@@ -5,7 +5,7 @@ use rand::distr::{Distribution, Uniform};
 pub async fn wait() {
     let jitter = {
         let mut rng = rand::rng();
-        Uniform::new(500, 1500).unwrap().sample(&mut rng)
+        Uniform::new(1000, 3500).unwrap().sample(&mut rng)
     }; // اینجا rng دراپ می‌شه و دیگه زنده نیست
 
     tokio::time::sleep(Duration::from_millis(jitter)).await;
